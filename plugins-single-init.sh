3482d92a91d3eab7d0988f5d0a3552ee6e342d53
Dima Minka <cdk@cdk.co.il>
2019-01-04 21:48:19 +0200

    feat--init--single plugin ready

--- plugins-single-init.sh
+++ plugins-single-init.sh
@@ -3,3 +3,39 @@
 # Init - Wp Pro Club
 # by DimaMinka (https://dimaminka.com)
 # https://github.com/wp-pro-club/init
+
+source ${PWD}/lib/app-init.sh
+
+version=""
+zip="^(https|git)(:\/\/|@)([^\/:]+)[\/:]([^\/:]+)\/([^\/:]+)\/([^\/:]+)\/(.+).zip$"
+# Get all single plugins and run install by type
+for i in "${!conf_app_plugins_single__name[@]}"
+do
+    printf "${GRN}======================================================${NC}\n"
+    printf "${GRN}Installing plugin ${conf_app_plugins_single__name[$i]}${NC}\n"
+    printf "${GRN}======================================================${NC}\n"
+    # Running plugin install via wp-cli
+    if [ "${conf_app_plugins_single__package[$i]}" == "wp-cli" ]; then
+        # Install from zip
+        if [[ ${conf_app_plugins_single__zip[$i]} =~ $zip ]]; then
+            wp plugin install ${conf_app_plugins_single__zip[$i]}
+        else
+            # Get plugin version from config
+            if [ "${conf_app_plugins_single__ver[$i]}" != "*" ]; then
+                version="--version=${conf_app_plugins_single__ver[$i]} --force"
+            fi
+            # Default plugin install via wp-cli
+            wp plugin install ${conf_app_plugins_single__name[$i]} ${version}
+        fi
+    elif [ "${conf_app_plugins_single__package[$i]}" == "wpackagist" ]; then
+        # Install plugin from wpackagist via composer
+        composer require wpackagist-plugin/${conf_app_plugins_single__name[$i]}:${conf_app_plugins_single__ver[$i]}
+    elif [ "${conf_app_plugins_single__package[$i]}" == "wp-pro-club" ]; then
+        ## Install plugin from private bitbacket repository via composer
+        project="${conf_app_plugins_single__package[$i]}/${conf_app_plugins_single__name[$i]}"
+        project_ver=${conf_app_plugins_single__ver[$i]}
+        project_zip="https://bitbucket.org/$project/get/$project_ver.zip"
+        composer config repositories.$project '{"type":"package","package": {"name": "'$project'","version": "'$project_ver'","type": "wordpress-plugin","dist": {"url": "'$project_zip'","type": "zip"}}}'
+        composer require $project:$project_ver
+    fi
+done
