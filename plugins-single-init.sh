c8001ab875565d234eb5281e607fba9d331e2ed0
DimaMinka <cdk@cdk.co.il>
2019-10-22 23:51:02 +0300

    feat--plugin-single--rebuild done

--- plugins-single-init.sh
+++ plugins-single-init.sh
@@ -25,6 +25,27 @@ do
   printf "${GRN}====================================================${NC}\n"
   printf "${GRN}Installing plugin $(wpi_yq plugins.single.[$i].name)${NC}\n"
   printf "${GRN}====================================================${NC}\n"
+  project=$(wpi_yq plugins.single.[$i].name)
+  project_ver=$(wpi_yq plugins.single.[$i].ver)
+  repo_name=$(echo ${project} | cut -d"/" -f2)
+  no_dev="--no-dev"
+  dev_commit=$(echo ${project_ver} | cut -d"#" -f1)
+  ver_commit=$(echo ${project_ver} | cut -d"#" -f2)
+
+  # Get plugin version from config
+  if [ "$project_ver" != "null" ] && [ "$project_ver" != "*" ]; then
+    json_ver=$project_ver
+    # check for commit version
+    if [ "$dev_commit" == "dev-master" ]; then
+      json_ver="dev-master"
+    fi
+  else
+    # default versions
+    json_ver="dev-master"
+    project_ver="dev-master"
+    ver_commit="master"
+  fi
+
   # Running plugin install via wp-cli
   if [ "$(wpi_yq plugins.single.[$i].package)" == "wp-cli" ]; then
     # Install from zip
@@ -32,19 +53,14 @@ do
       wp plugin install $(wpi_yq plugins.single.[$i].zip) --quiet
     else
       # Get plugin version from config
-      if [ "$(wpi_yq plugins.single.[$i].ver)" != "null" ] && [ "$(wpi_yq plugins.single.[$i].ver)" != "*" ]; then
-        version="--version=$(wpi_yq plugins.single.[$i].ver) --force"
+      if [ "$project_ver" != "null" ] && [ "$project_ver" != "*" ]; then
+        version="--version=$project_ver --force"
       fi
       # Default plugin install via wp-cli
       wp plugin install $(wpi_yq plugins.single.[$i].name) --quiet ${version}
     fi
   elif [ "$(wpi_yq plugins.single.[$i].package)" == "bitbucket" ]; then
     # Install plugin from private bitbucket repository via composer
-    project=$(wpi_yq plugins.single.[$i].name)
-    project_ver=$(wpi_yq plugins.single.[$i].ver)
-    repo_name=$(echo ${project} | cut -d"/" -f2)
-    no_dev="--no-dev"
-    
     # Check for setup settings
     if [ "$(wpi_yq plugins.single.[$i].setup)" != "null" ]; then
       name=$(wpi_yq plugins.single.[$i].setup)
@@ -60,54 +76,79 @@ do
       # Reset --no-dev
       no_dev=""
 
-      # Get plugin version from config
-      if [ "$(wpi_yq plugins.single.[$i].ver)" != "null" ] && [ "$(wpi_yq plugins.single.[$i].ver)" != "*" ]; then
-          version=$(wpi_yq plugins.single.[$i].ver)
-      else
-          version="dev-master"
-      fi
       # Composer config and install - GIT version
-      composer config repositories.$project '{"type":"package","package": {"name": "'$project'","version": "'$version'","type": "wordpress-plugin","source": {"url": "https://bitbucket.org/'$project'","type": "git","reference": "master"}}}'
-      composer require $project:$version --update-no-dev --quiet
+      composer config repositories.$project '{"type":"package","package": {"name": "'$project'","version": "'$json_ver'","type": "wordpress-plugin","source": {"url": "https://bitbucket.org/'$project'","type": "git","reference": "master"}}}'
+      composer require $project:$project_ver --update-no-dev --quiet
     else
       # Remove the package from composer cache
       if [ -d ~/.cache/composer/files/$project ]; then
         rm -rf ~/.cache/composer/files/$project
       fi
-      
-      # Get plugin version from config
-      if [ "$(wpi_yq plugins.single.[$i].ver)" != "null" ] && [ "$(wpi_yq plugins.single.[$i].ver)" != "*" ]; then
-          project_ver=$(wpi_yq plugins.single.[$i].ver)
-      else
-          project_ver="master"
-      fi
-      # Composer config and install - ZIP version      
-      project_zip="https://bitbucket.org/$project/get/$project_ver.zip"
+
+      # Composer config and install - ZIP version
+      project_zip="https://bitbucket.org/$project/get/$ver_commit.zip"
       composer config repositories.$project '{"type":"package","package": {"name": "'$project'","version": "'$project_ver'","type": "wordpress-plugin","dist": {"url": "'$project_zip'","type": "zip"}}}'
       composer require $project:$project_ver --update-no-dev --quiet
     fi
+  fi
 
-    # Check if setup exist
+  if [ "$(wpi_yq plugins.single.[$i].package)" == "github" ]; then
+    # Install plugin from private bitbucket repository via composer
+    project=$(wpi_yq plugins.single.[$i].name)
+    project_ver=$(wpi_yq plugins.single.[$i].ver)
+    repo_name=$(echo ${project} | cut -d"/" -f2)
+    no_dev="--no-dev"
+
+    # Check for setup settings
     if [ "$(wpi_yq plugins.single.[$i].setup)" != "null" ]; then
       name=$(wpi_yq plugins.single.[$i].setup)
-      composer=$(wpi_yq plugins.setup.$name.composer)
-      # Run install composer script in the plugin
-      if [ "$composer" != "null" ] && [ "$composer" == "install" ] || [ "$composer" == "update" ]; then
-        composer $composer -d ${PWD}/web/app/plugins/$repo_name $no_dev --quiet
-      elif [ "$composer" != "null" ] && [ "$composer" == "dump-autoload" ]; then
-        composer -d ${PWD}/web/app/plugins/$repo_name dump-autoload -o --quiet
+
+      # OAUTH for bitbucket via key and secret
+      if [ "$(wpi_yq plugins.setup.$name.github-token)" != "null" ] && [ "$(wpi_yq plugins.setup.$name.github-token)" != "null" ]; then
+        composer config -g github-oauth.github.com $(wpi_yq plugins.setup.$name.github-token)
       fi
     fi
 
-    # Run npm scripts
-    if [ "$(wpi_yq plugins.setup.$name.npm" != "null" ]; then
-      # run npm install
-      npm i --prefix ${PWD}/web/app/plugins/$repo_name
-      if [ "$cur_env" == "production" ] || [ "$cur_env" == "staging" ]; then
-        eval $(wpi_yq plugins.setup.$name.npm.prod) --prefix ${PWD}/web/app/plugins/$repo_name
-      else
-        eval $(wpi_yq plugins.setup.$name.npm.dev) --prefix ${PWD}/web/app/plugins/$repo_name
+    # Get GIT for local and dev
+    if [ "$cur_env" != "production" ] && [ "$cur_env" != "staging" ]; then
+      # Reset --no-dev
+      no_dev=""
+
+      # Composer config and install - ZIP version
+      composer config repositories.$project '{"type":"package","package": {"name": "'$project'","version": "'$json_ver'","type": "wordpress-plugin","source": {"url": "git@github.com:'$project'.git","type": "git","reference": "master"}}}'
+      composer require $project:$project_ver --update-no-dev --quiet
+    else
+      # Remove the package from composer cache
+      if [ -d ~/.cache/composer/files/$project ]; then
+        rm -rf ~/.cache/composer/files/$project
       fi
+
+      # Composer config and install - ZIP version
+      composer config repositories.$project '{"type":"package","package": {"name": "'$project'","version": "'$json_ver'","type": "wordpress-plugin","dist": {"url": "https://github.com/'$project'/archive/'$ver_commit'.zip","type": "zip","reference": "master"}}}'
+      composer require $project:$project_ver --update-no-dev --quiet
+    fi
+  fi
+
+  # Check if setup exist
+  if [ "$(wpi_yq plugins.single.[$i].setup)" != "null" ]; then
+    name=$(wpi_yq plugins.single.[$i].setup)
+    composer=$(wpi_yq plugins.setup.$name.composer)
+    # Run install composer script in the plugin
+    if [ "$composer" != "null" ] && [ "$composer" == "install" ] || [ "$composer" == "update" ]; then
+      composer $composer -d ${PWD}/web/app/plugins/$repo_name $no_dev --quiet
+    elif [ "$composer" != "null" ] && [ "$composer" == "dump-autoload" ]; then
+      composer -d ${PWD}/web/app/plugins/$repo_name dump-autoload -o --quiet
+    fi
+  fi
+
+  # Run npm scripts
+  if [ "$(wpi_yq plugins.setup.$name.npm)" != "null" ]; then
+    # run npm install
+    npm i --prefix ${PWD}/web/app/plugins/$repo_name
+    if [ "$cur_env" == "production" ] || [ "$cur_env" == "staging" ]; then
+      eval $(wpi_yq plugins.setup.$name.npm.prod) --prefix ${PWD}/web/app/plugins/$repo_name
+    else
+      eval $(wpi_yq plugins.setup.$name.npm.dev) --prefix ${PWD}/web/app/plugins/$repo_name
     fi
   fi
 done
