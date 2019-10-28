5e9d9ee2898a41683824dd5c02753485e1581b82
Dima Minka <bzx@mail.ru>
2019-10-28 20:30:55 +0200

    chore--bulk-init--rebuild in progress

--- plugins-bulk-init.sh
+++ plugins-bulk-init.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-# Init - Wp Pro Club
-# by DimaMinka (https://dimaminka.com)
-# https://github.com/wp-pro-club/init
+# WPI Buld Plugins
+# by DimaMinka (https://dima.mk)
+# https://github.com/wpi-pw/app
+
+# Get config files and put to array
+wpi_confs=()
+for ymls in wpi-config/*
+do
+  wpi_confs+=("$ymls")
+done
+
+# Get wpi-source for yml parsing, noroot, errors etc
+source <(curl -s https://raw.githubusercontent.com/wpi-pw/template-workflow/master/wpi-source.sh)
